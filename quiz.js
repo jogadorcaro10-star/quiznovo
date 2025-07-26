7e50230abe9fa28d5ef9053614bec905d6cca387
jogadorcaro10-star <alisom.fernandes@hotmail.com>
2025-07-26 16:52:46 -0300

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,16 @@
+function vperg1(botaoClicado, opcaoSelecionada){
+    if(opcaoSelecionada =='c1'){
+        var respCerta = document.getElementById('c1')
+        respCerta.style.backgroundColor = "green";
+        document.getElementById('resp1').innerHTML="✅Parabéns você acertou!"
+    } else {
+        var respCerta = document.getElementById('c1')
+        respCerta.style.backgroundColor = "green";
+        botaoClicado.style.backgroundColor = "red";
+         document.getElementById('resp1').innerHTML="❌Tente novamente"
+    }
+}
+function vperg2 (botaoClicado,opcaoSelecionada){
+    if {opcaoSelecionada =='b2'}
+    var respCerta = document.getElementById'
+}
