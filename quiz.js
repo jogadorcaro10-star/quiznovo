ca936f3740947955f6fec7f0623668ba85d07bb6
jogadorcaro10-star <alisom.fernandes@hotmail.com>
2025-07-28 18:06:21 -0300

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -10,7 +10,15 @@ function vperg1(botaoClicado, opcaoSelecionada){
          document.getElementById('resp1').innerHTML="❌Tente novamente"
     }
 }
-function vperg2 (botaoClicado,opcaoSelecionada){
-    if {opcaoSelecionada =='b2'}
-    var respCerta = document.getElementById'
+function vperg2(botaoClicado, opcaoSelecionada) {
+    if (opcaoSelecionada == 'b2') {
+        var respCerta = document.getElementById('b2')
+        respCerta.style.backgroundColor = "green";
+        document.getElementById('resp2').innerHTML = "✅Parabéns você acertou!"
+    } else {
+        var respCerta = document.getElementById('b2')
+        respCerta.style.backgroundColor = "green";
+        botaoClicado.style.backgroundColor = "red";
+        document.getElementById('resp2').innerHTML = "❌Tente novamente"
+    }
 }
